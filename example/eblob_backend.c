fee2ad1b2d7bf87ca6191181fee6c05af9892371
Kirill Bushminkin <kbushminkin@gmail.com>
2014-03-28 18:21:52 +0400

    pass-through original id

--- example/eblob_backend.c
+++ example/eblob_backend.c
@@ -42,6 +42,7 @@
 #include "elliptics/packet.h"
 #include "elliptics/interface.h"
 #include "elliptics/backends.h"
+#include "elliptics/original_id_helpers.h"
 
 #include "common.h"
 
@@ -126,7 +127,16 @@ static int blob_iterate_callback(struct eblob_disk_control *dc,
 			goto err;
 	}
 
-	err = ictl->callback(ictl->callback_private, (struct dnet_raw_id *)&dc->key,
+    struct dnet_raw_id key_elliptics_order, key_elliptics_transform;
+    memset(key_elliptics_order.id, 0, EBLOB_ID_SIZE);
+    reverse_copy_identifiers_cmd(key_elliptics_order.id, dc->key.id);
+
+    unsigned int transformed_size = DNET_ID_SIZE;
+    dnet_crypto_direct (key_elliptics_order.id, DNET_ID_SIZE, key_elliptics_transform.id, &transformed_size);
+
+    reverse_copy_identifiers_cmd(key_elliptics_transform.id, dc->key.id);
+
+    err = ictl->callback(ictl->callback_private, &key_elliptics_transform,
 			data, size, &elist);
 
 err:
@@ -189,7 +199,9 @@ static int blob_write(struct eblob_backend_config *c, void *state,
 	if (io->flags & DNET_IO_FLAGS_NOCSUM)
 		flags |= BLOB_DISK_CTL_NOCSUM;
 
-	memcpy(key.id, io->id, EBLOB_ID_SIZE);
+        // We are replacing straightforward copying for zeroing down the first bytes
+        // memcpy(key.id, io->id, EBLOB_ID_SIZE);
+        copy_identifiers_cmd (key.id, io->id);
 
 	if (io->flags & DNET_IO_FLAGS_PREPARE) {
 		err = eblob_write_prepare(b, &key, io->num + ehdr_size, flags);
@@ -297,7 +309,9 @@ static int blob_read(struct eblob_backend_config *c, void *state, struct dnet_cm
 	dnet_ext_list_init(&elist);
 	dnet_convert_io_attr(io);
 
-	memcpy(key.id, io->id, EBLOB_ID_SIZE);
+        // We are replacing straightforward copying for zeroing down the first bytes
+        // memcpy(key.id, io->id, EBLOB_ID_SIZE);
+        copy_identifiers_cmd (key.id, io->id);
 
 	if (io->flags & DNET_IO_FLAGS_NOCSUM)
 		csum = EBLOB_READ_NOCSUM;
@@ -475,9 +489,10 @@ static int blob_read_range_callback(struct eblob_range_request *req)
 			io.offset += sizeof(struct dnet_ext_list_hdr);
 			io.size -= sizeof(struct dnet_ext_list_hdr);
 		}
-
-		memcpy(io.id, req->record_key, DNET_ID_SIZE);
-		memcpy(io.parent, req->end, DNET_ID_SIZE);
+		//memcpy(key.id, cmd->id.id, EBLOB_ID_SIZE);
+		copy_identifiers_cmd (io.id, req->record_key);
+		//memcpy(key.id, cmd->id.id, EBLOB_ID_SIZE);
+		copy_identifiers_cmd (io.id, req->end);
 
 		err = dnet_send_read_data(p->state, p->cmd, &io, NULL, req->record_fd,
 				req->record_offset + io.offset, 0);
@@ -500,7 +515,8 @@ static int blob_del_range_callback(struct eblob_backend_config *c, struct eblob_
 	dnet_backend_log(c->blog, DNET_LOG_DEBUG, "%s: EBLOB: blob-read-range: DEL\n",
 			dnet_dump_id_str(req->record_key));
 
-	memcpy(key.id, req->record_key, EBLOB_ID_SIZE);
+	//memcpy(key.id, cmd->id.id, EBLOB_ID_SIZE);
+	copy_identifiers_cmd (key.id, req->record_key);
 	err = eblob_remove(req->back, &key);
 	if (err) {
 		dnet_backend_log(c->blog, DNET_LOG_DEBUG, "%s: EBLOB: blob-read-range: DEL: err: %d\n",
@@ -584,8 +600,10 @@ static int blob_read_range(struct eblob_backend_config *c, void *state, struct d
 
 	memset(&req, 0, sizeof(req));
 
-	memcpy(req.start, io->id, EBLOB_ID_SIZE);
-	memcpy(req.end, io->parent, EBLOB_ID_SIZE);
+	//memcpy(req.start, io->id, EBLOB_ID_SIZE);
+	copy_identifiers_cmd (io->id, req.start);
+	//memcpy(req.end, io->parent, EBLOB_ID_SIZE);
+	copy_identifiers_cmd (req.end, io->parent);
 	req.requested_offset = io->offset;
 	req.requested_size = io->size;
 	req.requested_limit_start = 0;
@@ -659,7 +677,8 @@ static int blob_del(struct eblob_backend_config *c, struct dnet_cmd *cmd)
 	struct eblob_key key;
 	int err;
 
-	memcpy(key.id, cmd->id.id, EBLOB_ID_SIZE);
+        //memcpy(key.id, cmd->id.id, EBLOB_ID_SIZE);
+        copy_identifiers_cmd (key.id, cmd->id.id);
 
 	err = eblob_remove(c->eblob, &key);
 	if (err) {
@@ -685,7 +704,8 @@ static int blob_file_info(struct eblob_backend_config *c, void *state, struct dn
 
 	dnet_ext_list_init(&elist);
 
-	memcpy(key.id, cmd->id.id, EBLOB_ID_SIZE);
+        //memcpy(key.id, cmd->id.id, EBLOB_ID_SIZE);
+        copy_identifiers_cmd (key.id, cmd->id.id);
 	err = eblob_read_return(b, &key, EBLOB_READ_NOCSUM, &wc);
 	if (err < 0) {
 		dnet_backend_log(c->blog, DNET_LOG_ERROR, "%s: EBLOB: blob-file-info: info-read: %d: %s.\n",
@@ -739,7 +759,8 @@ static int eblob_backend_checksum(struct dnet_node *n, void *priv, struct dnet_i
 	static const size_t ehdr_size = sizeof(struct dnet_ext_list_hdr);
 	int err;
 
-	memcpy(key.id, id->id, EBLOB_ID_SIZE);
+        //memcpy(key.id, cmd->id.id, EBLOB_ID_SIZE);
+        copy_identifiers_cmd (key.id, id->id);
 	err = eblob_read_return(b, &key, EBLOB_READ_NOCSUM, &wc);
 	if (err < 0) {
 		dnet_backend_log(c->blog, DNET_LOG_ERROR, "%s: EBLOB: blob-checksum: read: %d: %s.\n",
---
+++ include/elliptics/original_id_helpers.h
@@ -0,0 +1,39 @@
+#ifndef __ORIGINAL_ID_HELPERS_H
+#define __ORIGINAL_ID_HELPERS_H
+
+#include <eblob/blob.h>
+#include "elliptics/packet.h"
+#include "elliptics/interface.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+static inline void copy_identifiers_cmd (uint8_t *src_key, uint8_t* original_id)
+{
+    memset((void*)src_key, 0, EBLOB_ID_SIZE);
+    int id_size = sizeof (uint64_t);
+    int i, j;
+    for (i = 0, j = EBLOB_ID_SIZE - 1; i < id_size; i++, j--)
+    {
+        src_key [i] = original_id [j];
+    }
+}
+
+static inline void reverse_copy_identifiers_cmd (uint8_t *dst_key, uint8_t *src_key)
+{
+    int id_size = sizeof (int64_t);
+    int i, j;
+    for (i = 0, j = EBLOB_ID_SIZE - 1; i < id_size; i++, j--)
+    {
+        dst_key [j] = src_key [i];
+    }
+}
+
+int dnet_crypto_direct (const void *src, uint64_t size, void *dst, unsigned int *dsize);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __ORIGINAL_ID_HELPERS_H */
